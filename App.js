4686d6b8f41fd48eb689f9c1e12efb23f40b6a8b
abhishek Hegde <ahegde2021@gmail.com>
2023-09-12 19:30:33 +0530

    commit changes of lecture 4 part 1
    part 1 has completed . crfeated the layout and built the foundation .
    
    the restaurant card has been built but the  inner texts are haedcoded. now the next challenge would be how to make it dynamic?

--- App.js
+++ App.js
@@ -4,20 +4,67 @@ import ReactDOM from "react-dom/client";
 
 
 
+
+
+
 // create functional component Header
 
 const Header = ()=>{
     return(
         <div className="header">
-        {/* create a ul inside that have 4 list */}
-        <ul>
-            <li> Home </li>
-            <li> About </li>
-            <li> Contact </li>
-            <li> Login </li>
-        </ul>
+
+            <div className="logoContainer">
+                <img className="logo" src="https://imgs.search.brave.com/Mvy0HTvvOpSBRbEeJsdi-dox4fiGkzrUoRCLV7SvWsg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0LzM2LzUxLzUx/LzM2MF9GXzQzNjUx/NTE0OV83QjlaZDho/bjZIYk13bndLaUZk/Y2JwQ0o3SXF3dkR1/cy5qcGc" alt="Logo" />
+            </div>
+
+            <div className="nav-items">
+
+                <ul>
+                    <li> Home </li>
+                    <li> About </li>
+                    <li> Contact </li>
+                    <li> Login </li>
+                </ul>
+
+            </div>
+            {/* create a ul inside that have 4 list */}
+
         </div>
     )
+}
+
+const RestaurantCard = () =>{
+    return(
+        // it should have image , name of restaurant , star rating , cuisines , delvary tie
+        <div className="res-card">
+            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/usjgacltnt2rwla0hcpl" alt="pizza" />
+            <h3>Meghana Foods</h3>
+            <h4>biriyani , bengakuru</h4>
+            <h4>4.8 stars</h4>
+            <h4>39 minutes</h4>
+        </div>
+
+    )
+
+}
+
+
+
+const Body = () =>{
+    return(
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+                <RestaurantCard/><RestaurantCard/><RestaurantCard/><RestaurantCard/>
+                <RestaurantCard/><RestaurantCard/><RestaurantCard/><RestaurantCard/>
+                <RestaurantCard/><RestaurantCard/><RestaurantCard/><RestaurantCard/>
+                <RestaurantCard/><RestaurantCard/><RestaurantCard/><RestaurantCard/>
+                <RestaurantCard/><RestaurantCard/><RestaurantCard/><RestaurantCard/>
+            </div>
+        </div>
+    );
 }
 
 // create a function component AppLayout
@@ -26,13 +73,13 @@ const AppLayout = ()=>{
     return(
         <div className="app">
             <Header/>
-        <h1> Hello World </h1>
+            <Body/>
         </div>
     )
 }
 
 
-//  creating normal react elemnt i.e not component. (inside this)
+ {/* creating normal react elemnt i.e not component. (inside this) */}
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -42,7 +89,3 @@ root.render(<AppLayout/>)
 
 
 
-
-
-
-  
